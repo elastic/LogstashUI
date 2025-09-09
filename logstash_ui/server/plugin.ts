d6780901b34c647bd19b507d9f56316b346c0bc8
Codyjackson0321 <cody.jackson0321@gmail.com>
2025-09-08 21:47:41 -0400

    moving around plugin / route for logstash API calls

--- logstash_ui/server/plugin.ts
+++ logstash_ui/server/plugin.ts
@@ -9,39 +9,26 @@ import type {
 import type { LogstashUiPluginSetup, LogstashUiPluginStart } from './types';
 import { defineRoutes } from './routes';
 
-
-
-
-export class LogstashUiPlugin implements Plugin<LogstashUiPluginSetup, LogstashUiPluginStart> {
+export class LogstashUiPlugin
+  implements Plugin<LogstashUiPluginSetup, LogstashUiPluginStart>
+{
   private readonly logger: Logger;
 
   constructor(initializerContext: PluginInitializerContext) {
     this.logger = initializerContext.logger.get();
   }
 
-setup(core: CoreSetup) {
-  core.capabilities.registerProvider(() => ({
-    logstashUi: {
-      show: true,
-      save: true,
-    },
-  }));
+  setup(core: CoreSetup) {
+    core.capabilities.registerProvider(() => ({
+      logstashUi: { show: true, save: true },
+    }));
 
-  core.http.createRouter().get(
-    {
-      path: '/api/logstash_ui/pipelines',
-      validate: false,
-      options: {
-        tags: ['access:logstashUi'],
-      },
-    },
-    async (context, req, res) => {
-      const esClient = context.core.elasticsearch.client.asCurrentUser;
-      const result = await esClient.search({ index: '.logstash', size: 50 });
-      return res.ok({ body: result.hits.hits.map(h => h._source) });
-    }
-  );
-}
+    // create a router and register routes defined in routes/index.ts
+    const router = core.http.createRouter();
+    defineRoutes(router);
+
+    this.logger.debug('logstashUi: Setup complete');
+  }
 
   public start(core: CoreStart) {
     this.logger.debug('logstashUi: Started');
