6b81c9838ae1061cfe3d4829e01879ef7894be65
Codyjackson0321 <cody.jackson0321@gmail.com>
2026-02-19 09:33:56 -0500

    Bumping version to 0.1.12

--- LogstashUI/LogstashUI/settings.py
+++ LogstashUI/LogstashUI/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 # Example: ALLOWED_HOSTS=yourdomain.com,www.yourdomain.com
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
-__VERSION__ = "0.1.11"
+__VERSION__ = "0.1.12"
 # Application definition
 
 INSTALLED_APPS = [
