0825d7eebcddf2895774955c3e8bc0374d21e636
Codyjackson0321 <cody.jackson0321@gmail.com>
2026-06-09 18:58:46 -0400

    refactor: change walk query warning to be more direct refactor: added system.location and system.contact to the generic system profile

--- src/logstashui/SNMP/static/js/snmp_profile_modal.js
+++ src/logstashui/SNMP/static/js/snmp_profile_modal.js
@@ -281,7 +281,7 @@ function showWalkWarning() {
         <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
       </svg>
       <div class="text-sm text-yellow-200">
-        You will get much better results using table queries instead of walk queries.
+        Walk queries are best for discovery, not ongoing monitoring. For production polling, use Get or Table queries to avoid noisy data and mapping growth.
       </div>
     `;
     walkContainer.parentElement.insertBefore(warning, walkContainer);
